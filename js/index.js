73c365644f354227ec48554fbfb4cc158a484b2e
15814398695@163.com <15814398695@163.com>
2017-04-16 01:58:59 +0800

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,94 @@
 /**
  * Created by Super on 2017/4/13.
  */
-//ÂÖ²¥
+//æŠ€èƒ½
+var skill=document.getElementById("skill");
+var d1=skill.getElementsByTagName("div")[1];
+var d2=d1.getElementsByTagName("div");
+console.log(d2);
+function rc1(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+function rc2(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+function rc3(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+function rc4(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+function rc5(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+function rc6(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+function rc7(){
+    var r=Math.floor(Math.random()*256);
+    var g=Math.floor(Math.random()*256);
+    var b=Math.floor(Math.random()*256);
+    return `rgb(${r},${g},${b})`;
+}
+skill.onmouseover=function(){
+    for(var i=0;i<d2.length;i++) {
+        if (d2[i].className == "h5") {
+            d2[i].style.width = "85%";
+            d2[i].tra
+            d2[i].style.background = rc1();
+        } else if (d2[i].className == "c3") {
+            d2[i].style.width = "85%";
+            d2[i].style.background = rc2();
+        } else if (d2[i].className == "js") {
+            d2[i].style.width = "80%";
+            d2[i].style.background = rc3();
+        } else if (d2[i].className == "jq") {
+            d2[i].style.width = "75%";
+            d2[i].style.background = rc4();
+        } else if (d2[i].className == "rct") {
+            d2[i].style.width = "60%";
+            d2[i].style.background = rc5();
+        } else if (d2[i].className == "bsp") {
+            d2[i].style.width = "70%";
+            d2[i].style.background = rc6();
+        } else {
+            d2[i].style.width = "45%";
+            d2[i].style.background = rc7();
+        }
+    }
+}
+
+
+
+//ï¿½Ö²ï¿½
 var product=document.getElementById("product");
 var pro = product.getElementsByTagName('div')[0];
 var div = pro.getElementsByTagName("div");
 var li = product.getElementsByTagName("li");
-var i=index=0; //ÖÐ¼äÁ¿£¬Í³Ò»ÉùÃ÷£»
+var img=product.getElementsByTagName("img");
+console.log(img);
+var i=index=0; //ï¿½Ð¼ï¿½ï¿½ï¿½ï¿½ï¿½Í³Ò»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 var play=null;
-console.log(product,pro,div,li);//»ñÈ¡
-//Í¼Æ¬ÇÐ»»£¬µ­Èëµ­³öÐ§¹ûÎÒÊÇÓÃ£¨transition: opacity .s linear£©×öµÄ,²»¾À½á¡¢¼òµ¥ ÔÚcssÀïÃæ
+console.log(product,pro,div,li);//ï¿½ï¿½È¡
+//Í¼Æ¬ï¿½Ð»ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ëµ­ï¿½ï¿½Ð§ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ã£ï¿½transition: opacity .s linearï¿½ï¿½ï¿½ï¿½ï¿½ï¿½,ï¿½ï¿½ï¿½ï¿½ï¿½á¡¢ï¿½ï¿½ ï¿½ï¿½cssï¿½ï¿½ï¿½ï¿½
 function show(a){
     for(i=0;i<li.length;i++) {
         li[i].className = '';
@@ -17,11 +96,22 @@ function show(a){
     }
     for(i=0;i<div.length;i++){
         //console.log(div.length);
-        div[i].style.opacity=0;
-        div[a].style.opacity=1;
+        div[i].className="";
+        div[a].className="current";
+    }
+    for(var r=0;r<img.length;r++){
+        img[r]=img[a];
+        img[r].onmouseover=function(){
+            img[a].style.width="1100px";
+            img[a].style.height="500px";
+        };
+        img[r].onmouseout=function(){
+            img[a].style.width="900px";
+            img[a].style.height="360px";
+        }
     }
 }
-//ÇÐ»»°´Å¥¹¦ÄÜ£¬ÏìÓ¦¶ÔÓ¦Í¼Æ¬
+//ï¿½Ð»ï¿½ï¿½ï¿½Å¥ï¿½ï¿½ï¿½Ü£ï¿½ï¿½ï¿½Ó¦ï¿½ï¿½Ó¦Í¼Æ¬
 for(i=0;i<li.length;i++){
     li[i].index=i;
     //console.log(i);
@@ -31,7 +121,7 @@ for(i=0;i<li.length;i++){
         clearInterval(play);
     }
 }
-//×Ô¶¯ÂÖ²¥
+//ï¿½Ô¶ï¿½ï¿½Ö²ï¿½
 function autoPlay(){
     play=setInterval(function(){
         index++;
@@ -47,4 +137,4 @@ product.onmouseover=function(){
 };
 product.onmouseout=function(){
     autoPlay();
-}
+};
